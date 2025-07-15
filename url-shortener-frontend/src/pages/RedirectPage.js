7f1289e8b224d8786ec628522750a41a521897e9
Jigoku <135861482+Arsenal17x@users.noreply.github.com>
2025-07-15 12:10:51 +0530

    some error fixes

--- url-shortener-frontend/src/pages/RedirectPage.js
+++ url-shortener-frontend/src/pages/RedirectPage.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Log } from '../logging-middleware/logger';
+import { Log } from '../logging-middleware/src/logger';
 import { Typography, CircularProgress, Container, Box } from '@mui/material';
 
 const RedirectPage = () => {
